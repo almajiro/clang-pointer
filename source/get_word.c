d54ca4ca80fe1b8fb14b118ef7c39dc258fdfda7
Kuroki Almajiro <kuroki@almajiro.net>
2017-12-25 08:05:37 +0900

    add get_word.c

---
+++ source/get_word.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int get_word(char *buf, int buf_size, FILE *fp)
+{
+	int len = 0;
+	int ch;
+
+	while((ch = getc(fp)) != EOF && !isalnum(ch))
+		;
+
+	if(ch == EOF)
+		return EOF;
+
+	do{
+		buf[len] =  ch;
+		len++;
+
+		if(len >= buf_size){
+			fprintf(stderr, "word too long.\n");
+			exit(1);
+		}
+
+	}while((ch = getc(fp)) != EOF && isalnum(ch));
+	
+	buf[len] = '\0';
+
+	return len;
+}
+
+int main(void)
+{
+	char buf[256];
+
+	while(get_word(buf, 256, stdin) != EOF){
+		printf("<<%s>>\n", buf);
+	}
+
+	return 0;
+}
+
+
